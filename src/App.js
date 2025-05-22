fa36fc12ec6cf6311e56ab0ece2c3e55b047614b
kifathsala <kifathsala@gmail.com>
2025-05-22 11:15:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to Kifath Sala</h1>
+      <p>Your store for Art, Decor & More</p>
+      <a href="mailto:kifathsala@gmail.com" style={{ marginRight: '20px' }}>Email Us</a>
+      <a href="https://wa.me/91xxxxxxxxxx">WhatsApp</a>
+    </div>
+  );
+}
+
+export default App;
